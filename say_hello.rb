61f1fc57e3eec66dd1fc86a252a5c5434fa4661b
Chana Kupetz <chanakupetz@gmail.com>
2019-04-14 19:24:50 +0000

    Automatically backed up by Learn

--- say_hello.rb
+++ say_hello.rb
@@ -1,3 +1,9 @@
+<<<<<<< HEAD
 def say_hello(name = "Ruby Programmer")
   puts "Hello " + name + "!"
 end
+=======
+def say_hello(name = "Ruby Programmer")
+  puts "Hello, #{name}!"
+end
+>>>>>>> b395a70b737726c5e4a908ffa21606c5cb8c74e1
